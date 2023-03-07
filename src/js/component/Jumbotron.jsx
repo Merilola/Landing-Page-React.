9c74c3aac4f984b75df4f873d77ef2796029c551
Merilola <mariahef12@hotmail.com>
2023-03-07 00:56:10 +0000

    cambios2

--- src/js/component/Jumbotron.jsx
+++ src/js/component/Jumbotron.jsx
@@ -2,14 +2,16 @@ import React from "react";
 
 
 const Jumbotron = () => {
-    return (
-        <>
-<div className="jumbotron m-5 bg-secundary">
-  <h1 className="display-4 ">Welcome to Wikipedia</h1>
-  <p className="lead ">Wikipedia · It is an encyclopedia, understood as support that allows the collection, storage and transmission of information in a structured way.</p>
-  <a className="btn btn-dark btn-lg" href="https://es.wikipedia.org/wiki/Wikipedia" role="button">Go to the official website</a>
-</div>
-        </>
+  return (
+   <>
+      <div className="jumbotron m-5  ">
+        <h1 className="display-4 ">Welcome to Wikipedia</h1>
+        <p className="lead ">Wikipedia · It is an encyclopedia, understood as support that allows the collection, storage and transmission of information in a structured way.</p>
+ 
+        <a type="button" href="https://es.wikipedia.org/wiki/Wikipedia" class="btn btn-outline-secondary text-dark">Go to Wikipedia</a>
+      </div>
+    </>
     )
-}; 
+};
+
 export default Jumbotron;
--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -2,33 +2,32 @@ import React from "react";
 
 const Navbar = () => {
     return(
-        <>
+     <>
         <nav className="navbar navbar-expand-lg bg-dark">
-  <div className="container-fluid">
-    <a className="navbar-brand text-light" href="#">Wikkipedia</a>
-    <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <nav className="navbar-nav container-fluid ">
-    <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-      <ul className="navbar-nav ">
-        <li className="nav-item">
-          <a className="nav-link active text-light" aria-current="page" href="#">About</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link text-light" href="#">Services</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link text-light" href="#">Contact</a>
-        </li>
-      
-      </ul>
-    </div>
-    </nav>
-  </div>
-</nav>
+          <div className="container-fluid">
+                <a className="navbar-brand text-light" href="#">Wikipedia</a>
+                 <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                   <span className="navbar-toggler-icon"></span>
+                 </button>
+             <nav className="navbar-nav container-fluid ">
+                  <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul className="navbar-nav ">
+                      <li className="nav-item">
+                         <a className="nav-link active text-light" aria-current="page" href="#">About</a>
+                      </li>
+                      <li className="nav-item">
+                         <a className="nav-link text-light" href="#">Services</a>
+                      </li>
+                      <li className="nav-item">
+                         <a className="nav-link text-light" href="#">Contact</a>
+                      </li>
+                   </ul>
+                  </div>
+               </nav>
+           </div>
+         </nav>
 
-        </>
+     </>
     )
 };
 
